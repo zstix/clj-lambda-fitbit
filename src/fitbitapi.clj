6aecded33e3e8d62de4ecb6516c7c97145fd03f6
Zack Stickles <zstickles@gmail.com>
2021-02-05 20:28:36 -0800

    chore: adjusting request format

--- src/fitbitapi.clj
+++ src/fitbitapi.clj
@@ -4,13 +4,17 @@
   (:import java.util.Base64))
 
 (defn get-auth []
+  "Base64 encoded string `client_id:client_secret`"
   (.encodeToString
    (Base64/getEncoder)
    (.getBytes (str (environ/env :fitbit-client-id) ":"
                    (environ/env :fitbit-client-secret)))))
                                                       
 (defn get-token []
-  (client/get "https://api.fitbit.com/oauth2/token"
+  (client/post "https://api.fitbit.com/oauth2/token"
               {:basic-auth (get-auth)
-               ;; TODO: remaining part of request
-               }))
+               :form-params {:code ""
+                             :grant_type ""}
+               :async? true}
+              (fn [resp] (println "response: " resp))
+              (fn [exception] (println "exception: " (.getMessage exception)))))
